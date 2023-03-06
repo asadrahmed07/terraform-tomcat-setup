2c9f42cbd7e57217e526c71d5e6619596b2b4232
asadrahmed07 <asadrahmed@gmail.com>
2023-03-06 16:55:14 +0530

    basic Terraform project

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-provider "tomcat" {
-  url      = "http://localhost:8080"
-  username = "root"
-  password = "omniscient"
+resource "local_file" "example" {
+  content  = "Hello, World!"
+  filename = "example.txt"
 }
-
-resource "tomcat_webapp" "firestart2" {
-  name     = "firestart2"
-  war_path = "C:/terrformProject/firestart2.war"
-}
-
